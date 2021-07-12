6f3b2473cd47c292e8d887ad3e815d28fd667a42
Siddhi Pevekar <pevekarsiddhi@gmail.com>
2021-07-12 15:20:24 +0530

    remove useeffect

--- src/modals/EditTask.js
+++ src/modals/EditTask.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const EditTaskPopup = ({modal, toggle, updateTask, taskObj}) => {
